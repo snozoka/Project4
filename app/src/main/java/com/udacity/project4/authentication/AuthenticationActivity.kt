ff37b09b9e274267e19faf8c71f5599d3372b6d5
Noble S. Ozoka <snozoka@gmail.com>
2022-03-26 10:35:59 -0400

    Added login with error

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,21 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.map
+import com.google.firebase.auth.FirebaseAuth
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,9 +23,19 @@ import com.udacity.project4.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+    private val viewModel by viewModels<LoginViewModel>()
+
+    companion object {
+        const val TAG = "AuthenticationActivity"
+        const val SIGN_IN_RESULT_CODE = 1001
+    }
+    var buttonLogin = findViewById<Button>(R.id.buttonLogin)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
+
+        buttonLogin.setOnClickListener { launchSignInFlow() }
 //         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
 
 //          TODO: If the user was authenticated, send him to RemindersActivity
@@ -21,4 +44,73 @@ class AuthenticationActivity : AppCompatActivity() {
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
     }
+
+    private fun launchSignInFlow() {
+        // Give users the option to sign in / register with their email or Google account.
+        // If users choose to register with their email,
+        // they will need to create a password as well.
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+
+            // This is where you can provide more ways for users to register and
+            // sign in.
+        )
+
+        // Create and launch sign-in intent.
+        // We listen to the response of this activity with the
+        // SIGN_IN_REQUEST_CODE
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            SIGN_IN_RESULT_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_RESULT_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // User successfully signed in
+                Log.i(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
+    }
+
+    private fun observeAuthenticationState() {
+
+
+        viewModel.authenticationState.observe(this , Observer { authenticationState ->
+            // TODO 1. Use the authenticationState variable you just added
+            // in LoginViewModel and change the UI accordingly.
+            when (authenticationState) {
+                // TODO 2.  If the user is logged in,
+                // you can customize the welcome message they see by
+                // utilizing the getFactWithPersonalization() function provided
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                    val intent = Intent(this, RemindersActivity::class.java).apply {
+                    }
+                    startActivity(intent)
+//                    binding.authButton.setOnClickListener {
+//                        //AuthUI.getInstance().signOut(requireContext())
+//                    }
+                }
+                else -> {
+                    // TODO 3. Lastly, if there is no logged-in user,
+                    buttonLogin.setOnClickListener {
+                        launchSignInFlow()
+                    }
+                }
+            }
+        })
+    }
+
+
 }
---
+++ app/src/main/java/com/udacity/project4/authentication/FirebaseUserLiveData.kt
@@ -0,0 +1,28 @@
+package com.udacity.project4.authentication
+
+import androidx.lifecycle.LiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class FirebaseUserLiveData: LiveData<FirebaseUser?>() {
+    private val firebaseAuth = FirebaseAuth.getInstance()
+
+    // TODO set the value of this FireUserLiveData object by hooking it up to equal the value of the
+    //  current FirebaseUser. You can utilize the FirebaseAuth.AuthStateListener callback to get
+    //  updates on the current Firebase user logged into the app.
+    private val authStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+        value = firebaseAuth.currentUser
+    }
+
+    // When this object has an active observer, start observing the FirebaseAuth state to see if
+    // there is currently a logged in user.
+    override fun onActive() {
+        firebaseAuth.addAuthStateListener(authStateListener)
+    }
+
+    // When this object no longer has an active observer, stop observing the FirebaseAuth state to
+    // prevent memory leaks.
+    override fun onInactive() {
+        firebaseAuth.removeAuthStateListener(authStateListener)
+    }
+}
