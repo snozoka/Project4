f4dc352d630d84a670074ad4b28c600fd7ab3105
Noble S. Ozoka <snozoka@gmail.com>
2022-04-16 10:15:02 -0400

    Creating geofence

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -9,6 +9,7 @@ import com.udacity.project4.locationreminders.MainCoroutineRule
 import com.udacity.project4.locationreminders.data.FakeDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.getOrAwaitValue
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.MatcherAssert.assertThat
@@ -42,64 +43,54 @@ class SaveReminderViewModelTest {
     }
 
 
-//    //TODO: provide testing to the SaveReminderView and its live data objects
-//    @Test
-//    fun check_loading() {
-//        val reminder1 = ReminderDTO("Title1", "Description1","Location1",0.0,0.0)
-//        val reminder2 = ReminderDTO("Title2", "Description2", "Location2",0.1,0.1)
-//        val reminder3 = ReminderDTO("Title3", "Description3", "Location3",0.3,0.3)
-//
-//        // Pause dispatcher so you can verify initial values.
-//        mainCoroutineRule.pauseDispatcher()
-//
-//        // Load the reminders in the view model.
-//        addReminders(reminder1, reminder2, reminder3)
-//        //saveReminderViewModel.saveReminder()
-//
-//        // Then assert that the progress indicator is shown.
-//        assertThat(saveReminderViewModel.showLoading.getOrAwaitValue(), `is`(true))
-//        // Execute pending coroutines actions.
-//        mainCoroutineRule.resumeDispatcher()
-//
-//        // Then assert that the progress indicator is hidden.
-//        assertThat(saveReminderViewModel.showLoading.getOrAwaitValue(), `is`(false))
-//    }
-//
-//    @Test
-//    fun clearOldLiveData_readysNewLiveData(){
-//        //Given that a reminder =/live data objects have been saved
-//
-//        //When onClear() is called
-//
-//        //The live_data objects should all be null
-//    }
-//
-//    @Test
-//    fun validatesEnteredData_savesEnteredData() {
-//        //Given the user entered data
-//
-//        //When validateAndSaveReminder() is called
-//
-//        //The ReminderDataItem is saved to the data store
-//    }
-//
-//    @Test
-//    fun shouldReturnError(){
-//        //Make the data source return errors
-//        fakeDataSource.setReturnError(true)
-//
-//        //Then an error message is shown
-//        assertThat(saveReminderViewModel.reminderTitle.getOrAwaitValue(), `is`(true))
-//        assertThat(saveReminderViewModel.reminderSelectedLocationStr.getOrAwaitValue(), `is`(true))
-//    }
-//
-//    fun addReminders(vararg reminders: ReminderDTO) {
-//        for (reminder in reminders) {
-//            mainCoroutineRule.runBlockingTest {
-//                fakeDataSource.saveReminder(reminder)
-//            }
-//        }
-//    }
+    //TODO: provide testing to the SaveReminderView and its live data objects
+    @Test
+    fun check_loading() {
+        val reminder1 = ReminderDataItem("Title1", "Description1","Location1",0.0,0.0)
+        val reminder2 = ReminderDTO("Title2", "Description2", "Location2",0.1,0.1)
+        val reminder3 = ReminderDTO("Title3", "Description3", "Location3",0.3,0.3)
+
+        // Pause dispatcher so you can verify initial values.
+        mainCoroutineRule.pauseDispatcher()
+
+        // Load the reminders in the view model.
+        //addReminders(reminder1, reminder2, reminder3)
+        saveReminderViewModel.saveReminder(reminder1)
+
+
+        // Then assert that the progress indicator is shown.
+        assertThat(saveReminderViewModel.showLoading.getOrAwaitValue(), `is`(true))
+        // Execute pending coroutines actions.
+        mainCoroutineRule.resumeDispatcher()
+
+        // Then assert that the progress indicator is hidden.
+        assertThat(saveReminderViewModel.showLoading.getOrAwaitValue(), `is`(false))
+    }
+
+    @Test
+    fun shouldReturnError(){
+        //Given an entered reminder data
+        val reminder1 = ReminderDataItem("", "description1","",0.0,0.0)
+        //Make the data source return errors
+        fakeDataSource.setReturnError(true)
+
+        saveReminderViewModel.reminderTitle.value = reminder1.title.isNullOrEmpty().toString()
+        saveReminderViewModel.reminderSelectedLocationStr.value = reminder1.location.isNullOrEmpty().toString()
+
+        //saveReminderViewModel.validateAndSaveReminder(reminder1)
+
+        //Then an error message is shown
+        assertThat(saveReminderViewModel.reminderTitle.getOrAwaitValue(), `is`("true"))
+        assertThat(saveReminderViewModel.reminderSelectedLocationStr.getOrAwaitValue(), `is`("true"))
+    }
+
+    fun addReminders(vararg reminders: ReminderDTO) {
+        for (reminder in reminders) {
+            mainCoroutineRule.runBlockingTest {
+                fakeDataSource.saveReminder(reminder)
+            }
+        }
+    }
 
 
 }
