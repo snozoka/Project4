e2c1ab1d9ed9b55d95e16f0389397475d99cd896
Noble S. Ozoka <snozoka@gmail.com>
2022-04-30 19:08:01 -0400

    OnToast error message test working

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -18,6 +18,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.google.android.material.internal.ContextUtils.getActivity
+import com.udacity.project4.ToastMatcher.Companion.onToast
 import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
@@ -145,16 +146,13 @@ class RemindersActivityTest :
 
 
         //Test that the Toast message displays after the reminder is saved
-        onView(withText(R.string.reminder_saved)).inRoot(
-            withDecorView(
-                not(`is`(getActivity(appContext)?.window?.decorView!!))
-            )
-        ).check(matches(isDisplayed()))
+//        onView(withText(R.string.reminder_saved)).inRoot(
+//            withDecorView(
+//                not(`is`(getActivity(appContext)?.window?.decorView!!))
+//            )
+//        ).check(matches(isDisplayed()))
 
-        // Verify it was deleted
-//        onView(withId(R.id.menu_filter)).perform(click())
-//        onView(withText(R.string.nav_all)).perform(click())
-//        onView(withText("TITLE1")).check(doesNotExist())
+        onToast(R.string.reminder_saved).check(matches(isDisplayed()))
 
         // Make sure the activity is closed before resetting the db:
         activityScenario.close()
@@ -189,65 +187,5 @@ class RemindersActivityTest :
         activityScenario.close()
     }
 
-    @Test
-    fun testNavigationToRemindersListScreen() {
-        // Create a mock NavController
-        val mockNavController = mock(NavController::class.java)
-
-        // Create a graphical FragmentScenario for the TitleScreen
-        val titleScenario = launchFragmentInContainer<SaveReminderFragment>(Bundle(), R.style.AppTheme)
-
-        // Set the NavController property on the fragment
-        titleScenario.onFragment { fragment ->
-            Navigation.setViewNavController(fragment.requireView(), mockNavController)
-        }
-
-        // Verify that performing a click prompts the correct Navigation action
-        onView(withId(R.id.saveReminder)).perform(click())
-        verify(mockNavController).navigate(R.id.action_saveReminderFragment_to_reminderListFragment)
-        //verify(mockNavController).navigate(R.id.action_saveReminderFragment_to_selectLocationFragment)
-    }
-
-//    @Test
-//    fun happyPath_reminderSaved() = runBlocking {
-//        // Set initial state.
-//        repository.saveReminder(ReminderDTO("TITLE1", "DESCRIPTION", "Location1",-33.8523341,151.2106085))
-//
-//        // Start up Reminder screen.
-//        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
-//        dataBindingIdlingResource.monitorActivity(activityScenario)
-//
-//        //saveReminderViewModel.reminderSelectedLocationStr.value = "Location1"
-//
-//
-//
-//        // Click on the save reminder button, add reminder, and save.
-//        onView(withId(R.id.addReminderFAB)).perform(click())
-//        onView(withId(R.id.reminderTitle)).perform(replaceText("NEW TITLE"))
-//        onView(withId(R.id.reminderDescription)).perform(replaceText("NEW DESCRIPTION"))
-//        onView(withId(R.id.selectedLocation)).check(matches(withText("Location1")))
-//        onView(withId(R.id.saveReminder)).perform(click())
-//        onView(withId(com.google.android.material.R.id.snackbar_text))
-//            .check(matches(withText(R.string.err_enter_title)))
-//        onView(withId(com.google.android.material.R.id.snackbar_text))
-//            .check(matches(withText(R.string.err_select_location)))
-//
-//
-//        //Test that the Toast message displays after the reminder is saved
-//    onView(withText(R.string.reminder_saved)).inRoot(
-//            withDecorView(
-//                not(`is`(getActivity()?.window?.decorView!!))
-//            )
-//        ).check(matches(isDisplayed()))
-//
-//        // Verify task is displayed on screen in the reminder list.
-//        onView(withText("NEW TITLE")).check(matches(isDisplayed()))
-//        // Verify previous reminder is not displayed.
-//        onView(withText("TITLE1")).check(doesNotExist())
-//
-//
-//        // Make sure the activity is closed before resetting the db:
-//        activityScenario.close()
-//    }
 
 }
